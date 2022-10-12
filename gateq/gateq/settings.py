65020262d5780e10beb48a7e7b34ebe86ab495c9
ABZ187 <arbaazshaikh187@gmail.com>
2022-10-12 19:01:57 +0530

    probablity added

--- gateq/gateq/settings.py
+++ gateq/gateq/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8j)qq=@lwluw@pn+s3ns1k#8!tz(ics$$3dbt2qqz5dypn=%49'
-# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
